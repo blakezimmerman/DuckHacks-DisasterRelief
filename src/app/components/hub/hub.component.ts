00e42bf24f9c35ab217efd51e541b01afcfe7926
Connor Zapfel <czapfel@stevens.edu>
2017-03-26 07:46:41 -0400

    Added minimum code for locations section

--- src/app/components/hub/hub.component.ts
+++ src/app/components/hub/hub.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router, ActivatedRoute } from "@angular/router";
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'hub',
@@ -9,14 +10,19 @@ import { Router, ActivatedRoute } from "@angular/router";
 
 export class HubComponent {
   
-  constructor(private router: Router, private route: ActivatedRoute) {}
+  constructor(private router: Router, private route: ActivatedRoute, private sanitizer: DomSanitizer) {}
 
   boolAdvisories = 1;
   boolSurvivors = 1;
   boolContacts = 1;
+  boolLocations = 1;
 
   title = 'Disaster Info Page';
   advisories = [{"msg":"Travel ban in Hoboken"},{"msg":"State of Emergency in effect"}];
+  locations = [{"name":"Hoboken Terminal","address":"1 Hudson Pl Hoboken NJ","desc": "Primary rail/subway station for Hoboken"},
+               {"name":"City Hall","address":"94 Washington St Hoboken NJ", "desc":"Emergency Operations stationed in conference room here"}]
+  googleMapsLinks = ["https://www.google.com/maps/embed/v1/place?key=AIzaSyBGEAV3IlhXjIP7CW0MdhB1-h0NYXga8ic&q=94+Washington+Street+Hoboken+NJ",
+                     "https://www.google.com/maps/embed/v1/place?key=AIzaSyBGEAV3IlhXjIP7CW0MdhB1-h0NYXga8ic&q=1+Hudson+Place+Hoboken+NJ"]
   contacts = [{"name":"NJ OEM Lead","phone":"610-999-8789","email":"cbluk@memes.edu"},
               {"name":"Nigel Thornberry","phone":"911-341-6229","email":"eme@gmail.com"}];
   survivors = [{"firstName":"Matt","lastName":"Beans","midInit":"Y"},
