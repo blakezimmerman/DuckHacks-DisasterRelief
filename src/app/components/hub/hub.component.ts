43762413763243662daa641b8f483798e1a4ed7e
Connor Zapfel <czapfel@stevens.edu>
2017-03-26 06:52:08 -0400

    Contacts, Survivor and Advisory sections built

--- src/app/components/hub/hub.component.ts
+++ src/app/components/hub/hub.component.ts
@@ -11,7 +11,18 @@ export class HubComponent {
   
   constructor(private router: Router, private route: ActivatedRoute) {}
 
+  boolAdvisories = 1;
+  boolSurvivors = 1;
+  boolContacts = 1;
+
   title = 'Disaster Info Page';
+  advisories = [{"msg":"Travel ban in Hoboken"},{"msg":"State of Emergency in effect"}];
+  contacts = [{"name":"NJ OEM Lead","phone":"610-999-8789","email":"cbluk@memes.edu"},
+              {"name":"Nigel Thornberry","phone":"911-341-6229","email":"eme@gmail.com"}];
+  survivors = [{"firstName":"Matt","lastName":"Beans","midInit":"Y"},
+               {"firstName":"Kelsey","lastName":"Julier","midInit":"P"}]
+  siteName = "Hoboken's Hurricane Memey Page"
+  lastUpdate = "03/26/2017 @ 05:57 EST"    
 
   loadAdmin() {
      this.router.navigate(["/admin"]);
